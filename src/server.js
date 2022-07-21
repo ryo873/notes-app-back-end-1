c48e0f6a4f1acc04f1707a17b30a3994e39534b8
Ryo <ryoreinaldon11@gmail.com>
2022-07-21 07:30:26 +0700

    create comment in server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
 
-const init = async()=>{
+const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: "localhost",
     routes: {
       cors: {
-        origin: ['*'],
+        origin: ["*"],
       },
     },
   });
@@ -18,5 +18,6 @@ const init = async()=>{
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
-
 init();
+
+// testing
